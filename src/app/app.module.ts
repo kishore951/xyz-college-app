f7eaadbc565d282a9a7932bc5172c0bdd942e9f9
kishore951 <kishoresakthivel951@gmail.com>
2022-10-18 19:09:06 +0530

    two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const appRoutes:Routes=[
   {
@@ -14,7 +15,8 @@ const appRoutes:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
